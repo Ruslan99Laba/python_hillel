c797ce096c819b68e4deba898475e187270b3f3b
Ruslan99Laba <laba.ruslan99@gmail.com>
2023-05-01 15:39:11 +0300

    Homework

---
+++ main.py
@@ -0,0 +1,34 @@
+from random import randint
+
+from flask import Flask
+
+app = Flask(__name__)
+
+constant = [1, 2, 3]
+
+b = randint(0, 100)
+
+constant.append(b)
+
+del constant[-1]
+
+@app.route("/")
+def show_items():
+    return f"<h3>Number of books: {constant}!!!<h3>"
+
+
+@app.route("/add_item")
+def add_item():
+    return f"<h3>Number of books: {constant.append(b)}!!!<h3>"
+
+
+@app.route("/delete_item")
+def delete_item():
+    return f"<h3>Number of books: {constant}!!!<h3>"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8000, debug=True)
+
+
+
